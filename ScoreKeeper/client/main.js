af634905263eb860f95ea3c992023dbfe570ab46
Chris Iwasaki <ewahsocki@gmail.com>
2017-06-02 07:25:23 -0400

    Experimenting with API Callouts
    Experimenting with API Callouts

--- ScoreKeeper/client/main.js
+++ ScoreKeeper/client/main.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+	_id: '1',
+	name: 'Lauren',
+	score: 99
+  }, {
+	_id: '2',
+	name: 'Chris',
+	score: 60
+  }, {
+	_id: '3',
+	name: 'Roy',
+	score: 78
+}];
+
+const renderPlayers = function (playersList) {
+	//let numbers = [{val: 99}, {val: 2}, {val: 3}, {val:281203}];
+
+	return newNumbers = playersList.map(function (players) {
+		return <p key={players._id}>{players.name} has {players.score} points(s).</p>;
+	});
+};
+
 Meteor.startup(function () {
 	let title = 'Score Keep'
 	let name = 'Chris';
@@ -10,6 +32,7 @@ Meteor.startup(function () {
 		<h1>{title}</h1>
 		<p>Hello {name}</p>
 		<p>This is my second</p>
+		{renderPlayers(players)}
 	</div>);
 
 	ReactDOM.render(jsx, document.getElementById('app'));
--- ScoreKeeper/server/main.js
+++ ScoreKeeper/server/main.js
@@ -2,6 +2,52 @@
 import {greetUser} from './../imports/utils';
 import add from './../imports/math';
 
+//**********************************************************************
+//  Enter the Twitter handle below (var twitterHandle = 'BBCBreaking') to
+//  see a Sentiment Analysis of the first 10 tweets.
+//  :-) = positive, :-| = neutral, :-( = negative
+//**********************************************************************
+
+let AYLIENTextAPI = require('aylien_textapi'),
+  		request = require('request'),
+  		cheerio = require('cheerio');
+let textapi = new AYLIENTextAPI({
+  application_id: 'c6b987ff',
+  application_key: '13b7ba5303c8698dadd50c0edf66ce4f'
+});
+
+let twitterHandle = 'BBCBreaking';
+
+function sentimentToSmiley(sentiment) {
+  if(sentiment === "neutral") { return ':-|' }
+  if(sentiment === "negative") { return ':-(' }
+  else { return ':-)' }
+}
+
+function sentiment(text, callback) {
+	textapi.sentiment(text, function(err, resp) {
+  	if (err !== null) {
+    	console.log("Error: " + err);
+  	} else {
+    	callback(resp.polarity);
+  	}
+	});
+}
+
+request('https://www.twitter.com/' + twitterHandle, function(err, response) {
+  let $ = cheerio.load(response.body);
+  let i = 0;
+  $('.tweet-text').toArray().forEach(function(item) {
+    let text = $(item).text();
+    if(i < 10){
+      i++;
+    sentiment(text, function(results) {
+      console.log(sentimentToSmiley(results), '-', text.replace(/\n/g, ' '));
+    });
+
+    } else {return}
+
+  });
+});
+
 console.log('Log from /server/main.js');
-console.log(greetUser());
-console.log(add(12, 9));
