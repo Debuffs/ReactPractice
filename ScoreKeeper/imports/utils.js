cbf24ba9ca1211d92b6f50458e2a9422813f67ab
Chris Iwasaki <ewahsocki@gmail.com>
2017-06-05 01:02:55 -0400

    Adding Mongo
    Adding Mongo

--- ScoreKeeper/imports/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('Log from /imports/utils.js');
-
-export let greetUser = function () {
-	return 'Hello user!';
-}
-
-export let name = 'Chris Iwasaki';
-
-export default 'Default val';
--- ScoreKeeper/server/main.js
+++ ScoreKeeper/server/main.js
@@ -1,53 +1,14 @@
-//import './../imports/utils';
-import {greetUser} from './../imports/utils';
-import add from './../imports/math';
+import {Meteor} from 'meteor/meteor'
+import {Players} from './../imports/api/players';
 
-//**********************************************************************
-//  Enter the Twitter handle below (var twitterHandle = 'BBCBreaking') to
-//  see a Sentiment Analysis of the first 10 tweets.
-//  :-) = positive, :-| = neutral, :-( = negative
-//**********************************************************************
-
-let AYLIENTextAPI = require('aylien_textapi'),
-  		request = require('request'),
-  		cheerio = require('cheerio');
-let textapi = new AYLIENTextAPI({
-  application_id: 'c6b987ff',
-  application_key: '13b7ba5303c8698dadd50c0edf66ce4f'
-});
-
-let twitterHandle = 'BBCBreaking';
-
-function sentimentToSmiley(sentiment) {
-  if(sentiment === "neutral") { return ':-|' }
-  if(sentiment === "negative") { return ':-(' }
-  else { return ':-)' }
-}
-
-function sentiment(text, callback) {
-	textapi.sentiment(text, function(err, resp) {
-  	if (err !== null) {
-    	console.log("Error: " + err);
-  	} else {
-    	callback(resp.polarity);
-  	}
-	});
-}
-
-request('https://www.twitter.com/' + twitterHandle, function(err, response) {
-  let $ = cheerio.load(response.body);
-  let i = 0;
-  $('.tweet-text').toArray().forEach(function(item) {
-    let text = $(item).text();
-    if(i < 10){
-      i++;
-    sentiment(text, function(results) {
-      console.log(sentimentToSmiley(results), '-', text.replace(/\n/g, ' '));
-    });
-
-    } else {return}
-
+Meteor.startup(function () {
+  Players.insert({
+    name: 'Chris',
+    score: 3
   });
+  Players.insert({
+    name: 'Jean',
+    score: 600
+  });
+  console.log(Players.find().fetch());
 });
-
-console.log('Log from /server/main.js');
