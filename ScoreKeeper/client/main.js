a17a7f5856482f295fd6bb39d2565e5e5cbc4e33
Chris Iwasaki <ewahsocki@gmail.com>
2017-06-13 01:20:50 -0400

    Adding Javascropt Classes
    Adding Javascropt Classes

--- ScoreKeeper/client/main.js
+++ ScoreKeeper/client/main.js
@@ -6,14 +6,20 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 
 const renderPlayers = (playersList) => {
-	return newNumbers = playersList.map((players) => {
-		return <p key={players._id}>{players.name} has {players.score} points(s).</p>;
+	return playersList.map((players) => {
+		return (
+			<p key={players._id}>
+				{players.name} has {players.score} points(s).
+				<button onClick={() => Players.update(players._id, {$inc: {score: 1}})}>+1</button>
+				<button onClick={() => Players.update(players._id, {$inc: {score: -1}})}>-1</button>
+				<button onClick={() => Players.remove(players._id)}>X</button>
+			</p>
+		);
 	});
 };
-
 const handleSubmit = (e) => {
 	let playerName = e.target.playerName.value;
-	
+
 	e.preventDefault();
 
 	if(playerName) {
@@ -26,7 +32,6 @@ const handleSubmit = (e) => {
 }
 
 Meteor.startup(() => {
-
 	Tracker.autorun(() => {
 		let players = Players.find().fetch();
 		let title = 'Score Keep'
--- ScoreKeeper/server/main.js
+++ ScoreKeeper/server/main.js
@@ -2,7 +2,22 @@ import {Meteor} from 'meteor/meteor'
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
-
-  
-
+  class Person {
+    constructor(name = 'Anonymous', age = 0) {
+      this.name = name;
+      this.age = age;
+    }
+    getGreeting(){
+      // Hi! I am Chris.
+      //return 'Hi! I am ' + this.name + '.';
+      return `Hi! I am ${this.name}.`;
+    }
+    getPersonDescription(){r
+      return `${this.name} is ${this.age} year(s) old`;
+    }
+  }
+  let me = new Person('Chris');
+  console.log(me.getGreeting());
+  let person = new Person('Christopher', 15);
+  console.log(person.getPersonDescription());
 });
