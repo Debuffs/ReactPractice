ff6b62c64d1d8ca1487b2bed47182345ea49fe62
Chris Iwasaki <ewahsocki@gmail.com>
2017-06-14 04:31:55 -0400

    Adding Playerlist component
    Adding Playerlist component

---
+++ ScoreKeeper/imports/ui/PlayerList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Player from './Player';
+
+const renderPlayers = (playersList) => {
+
+};
+
+export default class PlayerList extends React.Component {
+  renderPlayers() {
+    return this.props.players.map((player) => {
+  		return <Player key={player._id} player={player}/>;
+  	});
+  }
+
+  render() {
+		return (
+      <div>
+        {this.renderPlayers()}
+      </div>
+    );
+
+	}
+}
+PlayerList/propTypes = {
+  players: React.PropTypes.array.isRequired
+}
