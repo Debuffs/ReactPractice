bb8744cec2460f8449ee9fe03f7d501649bfffce
Chris Iwasaki <ewahsocki@gmail.com>
2017-06-15 04:05:29 -0400

    Adding SCSS Styling
    Adding SCSS Styling

--- ScoreKeeper/imports/ui/Player.js
+++ ScoreKeeper/imports/ui/Player.js
@@ -5,12 +5,14 @@ import PropTypes from 'prop-types';
 export default class Player extends React.Component {
 	render() {
 		return (
-      <p key={this.props.player._id}>
-				{this.props.player.name} has {this.props.player.score} points(s).
+      <div key={this.props.player._id} className="item">
+				<p>
+					{this.props.player.name} has {this.props.player.score} points(s).
+				</p>
 				<button onClick={() => Players.update(this.props.player._id, {$inc: {score: 1}})}>+1</button>
 				<button onClick={() => Players.update(this.props.player._id, {$inc: {score: -1}})}>-1</button>
 				<button onClick={() => Players.remove(this.props.player._id)}>X</button>
-			</p>
+			</div>
 
     )
 	}
